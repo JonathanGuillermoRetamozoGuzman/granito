f51c3e03e7171520a69b31073163c6a6f9216655
JonathanGuillermoRetamozoGuzman <isseixd841@gmail.com>
2026-04-09 17:15:44 -0500

    Update horarios.js

--- api/horarios.js
+++ api/horarios.js
@@ -1,5 +1,7 @@
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate');
+  res.setHeader('Pragma', 'no-cache');
 
   const url = `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/Horarios`
     + `?sort[0][field]=Orden&sort[0][direction]=asc`;
